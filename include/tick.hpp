c7ba01c9161b94fc8657515d33b3b82e62d73317
miyatakazuya <kazuyamiyata18@gmail.com>
2025-08-31 09:40:10 +0900

    Added Base Tick Architecture Files

--- include/tick.hpp
+++ include/tick.hpp
@@ -1,48 +1,29 @@
+// include/ticks/Tick.hpp
 #ifndef INCLUDE_TICKS_TICK_HPP_
 #define INCLUDE_TICKS_TICK_HPP_
 
-#include <memory>
 #include <chrono>
-#include <string>
-#include <mutex>
+#include <memory>
 
-#include "core/mission_state.hpp"
-#include "ticks/ids.hpp"
-#include "utilities/logging.hpp"
+class MissionState; 
 
 class Tick {
- public:
-    Tick(std::shared_ptr<MissionState> state, TickID id) {
-        this->state = state;
-        this->id = id;
-        this->already_validated = false;
-    }
+public:
+    // The constructor takes the shared state object
+    explicit Tick(std::shared_ptr<MissionState> state);
     virtual ~Tick() = default;
 
-    // how long to wait between running each tick function
-    virtual std::chrono::milliseconds getWait() const = 0;
-
-    /**
-     * function that is called every getWaitTimeMS() miliseconds
-     * return nullptr if no state change should happen
-     * return new implementation of Tick if state change should happen
-     */
-    virtual Tick* tick() = 0;
-
-    /**
-     * Code that should be run upon entering the tick should be placed here, not in the constructor
-     * because the constructor can possibly be called before actually transitioning into the tick,
-     * as is the case for 
-     */
+    // Called once when the state is transitioned to
     virtual void init() {}
 
-    constexpr TickID getID() const { return this->id; }
-    constexpr const char* getName() const { return TICK_ID_TO_STR(this->id); }
+    // The main logic loop for the tick
+    virtual Tick* tick() = 0;
 
- protected:
+    // Controls how often tick() is called
+    virtual std::chrono::milliseconds getWait() const = 0;
+
+protected:
     std::shared_ptr<MissionState> state;
-    TickID id;
-    bool already_validated;
 };
 
-#endif  // INCLUDE_TICKS_TICK_HPP_
+#endif // INCLUDE_TICKS_TICK_HPP_
