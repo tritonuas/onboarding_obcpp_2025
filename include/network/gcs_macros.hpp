21dc33de456c82b6e4b0ce4873db82726f2980d9
Kimi Chen <kimichen1115@gmail.com>
2025-10-03 22:01:01 -0700

    Delete useless macro

--- include/network/gcs_macros.hpp
+++ include/network/gcs_macros.hpp
@@ -10,27 +10,4 @@
 #define GCS_HANDLE(Method, uri) Method##_##uri
 #define DEF_GCS_HANDLE(Method, uri) void GCS_HANDLE(Method, uri) (GCS_HANDLER_PARAMS)
 
-// Macro to convert ODLCObjects enum to string
-#define ODLCObjects_ID_TO_STR(detected_obj)                                  \
-    ((detected_obj).object() == ODLCObjects::Undefined      ? "Undefined"    \
-     : (detected_obj).object() == ODLCObjects::Mannequin    ? "Mannequin"    \
-     : (detected_obj).object() == ODLCObjects::Car          ? "Car"          \
-     : (detected_obj).object() == ODLCObjects::Motorcycle   ? "Motorcycle"   \
-     : (detected_obj).object() == ODLCObjects::Airplane     ? "Airplane"     \
-     : (detected_obj).object() == ODLCObjects::Bus          ? "Bus"          \
-     : (detected_obj).object() == ODLCObjects::Boat         ? "Boat"         \
-     : (detected_obj).object() == ODLCObjects::Stopsign     ? "Stopsign"     \
-     : (detected_obj).object() == ODLCObjects::Snowboard    ? "Snowboard"    \
-     : (detected_obj).object() == ODLCObjects::Umbrella     ? "Umbrella"     \
-     : (detected_obj).object() == ODLCObjects::SoccerBall   ? "SoccerBall"   \
-     : (detected_obj).object() == ODLCObjects::Basketball   ? "Basketball"   \
-     : (detected_obj).object() == ODLCObjects::Volleyball   ? "Volleyball"   \
-     : (detected_obj).object() == ODLCObjects::Football     ? "Football"     \
-     : (detected_obj).object() == ODLCObjects::Baseballbat  ? "Baseballbat"  \
-     : (detected_obj).object() == ODLCObjects::Mattress     ? "Mattress"     \
-     : (detected_obj).object() == ODLCObjects::Tennisracket ? "Tennisracket" \
-     : (detected_obj).object() == ODLCObjects::Suitcase     ? "Suitcase"     \
-     : (detected_obj).object() == ODLCObjects::Skis         ? "Skis"         \
-                                                            : "Unknown")
-
 #endif  // INCLUDE_NETWORK_GCS_MACROS_HPP_
